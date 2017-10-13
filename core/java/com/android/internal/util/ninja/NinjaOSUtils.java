7462906bd66fb1ae73472fa1efb3d866ef8fe9a0
Marko Man <darkobas@gmail.com>
2017-10-13 16:48:20 +0200

    PiP tile: add toast if pip mode is not avail
    Change-Id: Idfa864c7b26f7965afb233ae377b4a520da326a8

--- core/java/com/android/internal/util/ninja/NinjaOSUtils.java
+++ core/java/com/android/internal/util/ninja/NinjaOSUtils.java
@@ -16,7 +16,10 @@
 
 package com.android.internal.util.ninja;
 
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.hardware.input.InputManager;
 import android.os.Handler;
@@ -32,6 +35,8 @@ import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.statusbar.IStatusBarService;
+
+import java.util.List;
 
 /**
  * Some custom utilities
@@ -109,5 +114,21 @@ public class NinjaOSUtils {
                 }
             }
         }
+    }
+
+    public static ActivityInfo getRunningActivityInfo(Context context) {
+        final ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                return pm.getActivityInfo(top.topActivity, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return null;
     }
 }
